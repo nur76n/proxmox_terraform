55edcea9f961bcf8164743420f248f673a68baca
Administrator <admin@example.com>
2021-08-21 00:52:07 +0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.7.4"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://${var.pve_addr}:8006/api2/json"
+  pm_user         = var.pve_user
+  pm_password     = var.pve_password
+  pm_tls_insecure = true
+}
+
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+  count       = length(var.vms)
+  name        = "${var.vms[count.index].name}-${var.vms[count.index].number}"
+  desc        = "tf description"
+  target_node = "pve1"
+
+  clone = "ubuntu-cloud-init-template"
+
+  pool    = "tf"
+  boot    = "cdn"
+  hotplug = "network,disk,usb"
+
+  disk {
+    backup  = 0
+    type    = "scsi"
+    storage = "local-lvm-cached"
+    size    = "8G"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  cores   = 4
+  sockets = 1
+  memory  = 2048
+
+  os_type    = "cloud-init"
+  ciuser     = var.vm_user
+  cipassword = var.vm_password
+  ipconfig0  = "ip=${var.vms[count.index].ip_addr_pref}.${var.vms[count.index].number}/24,gw=10.8.1.1"
+  agent      = 1
+
+  sshkeys = join("\n", var.vm_public_ssh_keys)
+
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "pve_user" {
+  description = "pve user"
+  type        = string
+}
+
+variable "pve_password" {
+  description = "pve password"
+  type        = string
+}
+
+variable "pve_addr" {
+  description = "pve address"
+  type        = string
+}
+
+variable "vm_user" {
+  description = "vm user"
+  type        = string
+}
+
+variable "vm_password" {
+  description = "vm password"
+  type        = string
+}
+
+variable "vms" {
+  description = "List of resources to create"
+  type        = list(any)
+  default = [{ "number" = "85", "name" = "swarm", "ip_addr_pref" = "10.8.1" },
+  { "number" = "86", "name" = "swarm", "ip_addr_pref" = "10.8.1" }]
+}
+
+variable "vm_public_ssh_keys" {
+  description = "vm public_ssh_keys"
+  type        = list(string)
+}
